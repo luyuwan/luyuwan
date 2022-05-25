8fbd5b63d407f88777f6e980ac5ce06a71aa4b8c
luyuwan <78227387+luyuwan@users.noreply.github.com>
2022-05-25 13:56:45 +0800

    Update jiancejiesuo.js

--- jiancejiesuo.js
+++ jiancejiesuo.js
@@ -22,7 +22,7 @@ const UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (
     let panel_result = {
       title: '流媒体解锁检测',
       content: '',
-      icon: 'play.tv.fill',
+      icon: 'lock.open.display',
       'icon-color': 'F61A1A',
     }
   let [{ region, status }] = await Promise.all([testDisneyPlus()])
